8f1d8fb738566fb5c232cb0dde1a83eb974cbe0a
anyangateny1 <a1853103@adelaide.edu.au>
2025-09-03 19:45:15 +0930

    refactor: rebase to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
 #include "block_model.h"
+#include <iostream>
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    BlockModel bm;
-    bm.read_specification();
-    bm.read_tag_table();
-    bm.read_model();
-    return 0;
-}
+  BlockModel bm;
+  bm.read_specification();
+  bm.read_tag_table();
+  bm.read_model();
+  return 0;
+} // Test comment
+// Test comment for pre-commit hook
