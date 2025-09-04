58e98584576bc332785d94be408a0981f4fe0154
anyangateny1 <a1853103@adelaide.edu.au>
2025-09-04 20:19:44 +0930

    feat: perm 4 thread version

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <cstdlib>
+#include <thread>
+#ifdef USE_TBB
+#include <tbb/global_control.h>
+#endif
 #include "block_model.h"
 
 int main() {
@@ -6,6 +11,15 @@ int main() {
     std::cin.tie(nullptr);
 
     BlockModel bm;
+    // Permanently use 4 threads
+    unsigned int thread_limit = 4u;
+
+    bm.set_num_threads(thread_limit);
+
+#ifdef USE_TBB
+    // Enforce TBB global thread cap for the duration of main
+    tbb::global_control tbb_limit(tbb::global_control::max_allowed_parallelism, thread_limit);
+#endif
     bm.read_specification();
     bm.read_tag_table();
     bm.read_model();
