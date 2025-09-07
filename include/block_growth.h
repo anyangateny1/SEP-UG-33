c405cc2c206217828ad0cebf36041114f30b3b7c
Patreeky-2011 <rajkarthick.raju824@gmail.com>
2025-09-07 15:36:13 +0930

    feat: Use 3D prefix sums to improve scanning times

--- include/block_growth.h
+++ include/block_growth.h
@@ -5,13 +5,14 @@
 #include <string>
 #include <unordered_map>
 #include "block.h"
+#include <optional>
 
 // 3D alias: [depth][height][width]
 template<typename T>
 using Vec3 = std::vector<std::vector<std::vector<T>>>;
 
 // BlockGrowth encapsulates the "fit & grow" compression logic for a parent block
-// over a sub-volume (model_slices). The tag_table maps single-char tags to labels.
+// over a sub-volume (model_sl  ices). The tag_table maps single-char tags to labels.
 class BlockGrowth {
 public:
     BlockGrowth(const Vec3<char>& model_slices,
@@ -26,6 +27,13 @@ private:
     const Vec3<char>& model; // sub-volume: depth x height x width
     const std::unordered_map<char, std::string>& tag_table;
 
+    //Prefix sum tables
+    std::vector<int> prefix_tag;
+    std::vector<int> prefix_compressed;
+    int D, H, W;
+    bool prefix_enabled = false; //control hybrid method. Prefix sums only for large blocks
+    int candidate_count = 0;
+
     // State for current run()
     Block parent_block{0,0,0,0,0,0,'\0'};
     int parent_x_end = 0, parent_y_end = 0, parent_z_end = 0;
@@ -39,6 +47,17 @@ private:
 
     Block fit_block(char mode, int width, int height, int depth);
     void grow_block(Block& current, Block& best_block);
+
+
+    //for the prefix sum tables
+    inline int idx(int z, int y, int x) const {
+        return (z * (H+1) * (W+1)) + (y * (W+1)) + x;
+    }
+    void build_prefix_tables(char tag);
+    int query_prefix(const std::vector<int>& psum,
+                     int z0, int y0, int x0,
+                     int z1, int y1, int x1) const;
+    void maybe_build_prefix(char tag, int num_queries);
 
     bool window_is_all(char val,
                        int z0, int z1, int y0, int y1, int x0, int x1) const;
